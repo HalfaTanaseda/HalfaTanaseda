f86cdfe4b30e3c58e736bbcd2fb7a528d1312bb5
HalfaTanaseda <98675305+HalfaTanaseda@users.noreply.github.com>
2022-01-30 03:12:40 +0100

    Add files via upload

---
+++ aula 7, incremento e decremento de variaveis.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+	
+	int n1 = 10, n2 = 10;
+	
+	
+	cout << n1 <<"\n\n";
+	//n1=n1+1;
+	//cout << n1 <<"\n\n";
+	
+	n1+=100;
+	cout << n1 <<"\n\n";
+	//n1++;
+	//cout << n1 <<"\n\n";
+	//cout << n1++ << "\n\n";
+	//cout << ++n1 <<"\n\n";
+	
+	return 0;
+}
